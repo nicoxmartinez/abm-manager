e4d4aa46c163f2994ee0c3f5cfc73641945c4b0f
Nicolas Martinez <nicolas0martinez1@gmail.com>
2026-09-12 18:20:23 -0300

    Initial commit

---
+++ src/main/java/com/ventas/abm/manager/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.ventas.abm.manager.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+	
+	@GetMapping
+    public ResponseEntity<String> getTest() {
+        return ResponseEntity.ok("Test de API OK");
+    }
+
+}
